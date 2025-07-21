67dfb907cf2ab2550b97c4018886c82d7cd26bec
kaciak <wlomcj0@gmail.com>
2025-07-21 15:28:42 +0200

    Add contact form with Formspree integration

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -22,7 +22,7 @@ export default function Project() {
     <div className=" max-w-[90rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto text-gray-800">
       <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
         <h2 className="text-2xl mt-4 font-bold md:text-4xl md:leading-tight">
-          Moje Projekty
+          Projects
         </h2>
       </div>
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 ">
